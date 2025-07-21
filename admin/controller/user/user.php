03e8f58c5edd430fcc6e136115acc2d7e6c214c8
Daniel <danielcabreraluque@gmail.com>
2025-07-21 10:29:52 -0300

    modif daniel

--- admin/controller/user/user.php
+++ admin/controller/user/user.php
@@ -169,6 +169,9 @@ class ControllerUserUser extends Controller {
 
 		$user_total = $this->model_user_user->getTotalUsers();
 
+		error_reporting(E_ALL);
+		ini_set('display_errors', '1');
+
 		$results = $this->model_user_user->getUsers($filter_data);
 
 		foreach ($results as $result) {
